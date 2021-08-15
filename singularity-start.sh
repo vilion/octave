cc432f5dededfebf25d113874295d4c0e96666ef
Koichi Takabatake <takabatakekoichi@gmail.com>
2021-08-15 13:54:22 +0900

    ubuntuで起動できる簡易スクリプト

---
+++ singularity-start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+singularity exec --bind /home/takabatakekoichi/study/machine-learning/coursera-andrew-ng/ex1-octave /home/takabatakekoichi/go/src/github.com/sylabs/singularity-ce-3.8.1/octave_6.3.0.sif octave --gui
---
+++ start-docker-ubuntu.sh
@@ -0,0 +1,1 @@
+HOSTNAME=$(hostname) NB_USER=$(id -u) NB_GID=$(id -g) docker-compose -f docker-compose-ubuntu.yml up
